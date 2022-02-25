190a7c33d7cfbc8f0781e952bf2270f4fad5a3a8
devdevgoat <russell.w.lamb@gmail.com>
2022-02-25 15:24:57 -0500

    prapha deployed to ipfs
    this version is deployed here:
    https://ipfs.io/ipfs/QmfLxmHpwksrFcHn9uq9R1ED9YoifpCiSnp8u74LDuMzje?filename=index.html
    
    some bugs of course, but it's kinda working!

--- public/scratch.js
+++
@@ -1,4 +0,0 @@
-myContract.methods.myMethod(123).send({from: '0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe'})
-.then(function(receipt){
-    // receipt can also be a new contract instance, when coming from a "contract.deploy({...}).send()"
-});
--- src/bin/2_ipfs.js
+++ src/bin/2_ipfs.js
@@ -8,6 +8,10 @@ var Contract = require('web3-eth-contract');
 var acts;
 var abi = require('./osABI.json');
 window.abi = abi
+// var contractAddress = '0xe97dE59010A39fE0913A897F5C205a85122D6b59'
+// var contractAddress = '0xAc40865dbCbfE2931dc1aF3538916fC3d1add20F'
+// var contractAddress = '0x0098A795096B23a8d8965Fd231778642d26C4Adb'
+var contractAddress = '0xECCd8F29E44E441aE9F9b976E71Ff95DCe8a53FD'
 
 var web3 = new Web3(Web3.givenProvider)
 
@@ -22,7 +26,7 @@ const ethEnabled = async () => {
         // window.web3.eth.defaultAccount = acts[0];
       return true;
     } else{
-      alert('Not a Web3 enabled browser!')
+      alert({"message":'Not a Web3 enabled browser!'})
     }
     return false;
   }
@@ -102,46 +106,82 @@ const ethEnabled = async () => {
   }
 
   const populateFeed = async () => {
-    if(!await isRegistered()) {
-      console.log("Register first");
-      return;
-    } else {
-      console.log("User is registered, loading feed")
-      try {
-        if (hasAlert) hideAlert()
-        var postCount = await window.contract.methods.countOfPosts(defaultAccount).call();
+    if(!await isRegistered()) return;
+  
+    console.log("User is registered, loading feed")
+    try {
+      if (hasAlert) hideAlert()
+
+      var followersAndSelf = await getFollowingAccounts()
+      // console.log("got followers... or lack there of")
+      for (let _user in followersAndSelf){
+          console.log("Getting post from user ",followersAndSelf[_user])
+          await getPostsOfFollowersAndSelf(followersAndSelf[_user])
+      }
+
+    } catch (err) {
+      console.log(err)
+      alert(err)
+    }
+    
+  // need to add an emit to the contract now and listen for new posts
+  }
+
+  const getFollowingAccounts = async () => {
+    if(!await isRegistered()) return;
+    var followingCount = await window.contract.methods.countOfFollowing().call({"from":window.defaultAccount});
+    var following = [window.defaultAccount];
+    if (following == 0) return following;
+    for (let i = 0; i<followingCount; i++){
+      following.push(
+        await window.contract.methods.getFollowingUser(i).call({"from":window.defaultAccount})
+      );
+    }
+    return following;
+  }
+
+  const getPostsOfFollowersAndSelf = async (_userAddress) => {
+    // https://stackoverflow.com/questions/51847788/msg-sender-does-not-work-inside-a-view-function-why-is-there-a-workaround
+        // "It appears you're running a call, which runs 
+        //    quickly and does not alter the state of the blockchain. msg.sender 
+        //    is set in both a transaction and a call. In a transaction, it cannot 
+        //    be faked: you must have the private key associated 
+        //    with the given account. But in a call, you are free to set the sender to any value you like."
+        // Thus, we need to set {"from":defaultAccount} in all call methods
+        var postCount = await window.contract.methods.countOfPosts(_userAddress).call({"from":window.defaultAccount});
         let feed = DOM.feed();
         for (let i = 0; i<postCount; i++){
-            var ipfsDoc = await window.contract.methods.postsOfUser(defaultAccount, i).call();
+            var ipfsDoc = await window.contract.methods.getUserPost(_userAddress, i).call({"from":window.defaultAccount});
             console.log(`Found post ${ipfsDoc.CID}`);
             if (!document.body.contains(document.getElementById(ipfsDoc.CID))){
-              var div = await createFeedEntry(defaultAccount,ipfsDoc.CID);
+              var div = await getFeedEntry(window.defaultAccount,ipfsDoc.CID);
               feed.prepend(div);
             }
         }
-      } catch (err) {
-        console.log(err)
-        alert(err)
-      }
-      
-    }
-    // need to add an emit to the contract now and listen for new posts
   }
 
+  const createEventEntry = async ( content ) => {
+    return await constructFeedEntry('Contract Event','0x0Event', 'bell.png', content)
+  }
 
-  const createFeedEntry = async (account, cid ) => {
+  const getFeedEntry = async (account, cid, imgCid) => {
       var content = await cat(cid);
+      var img = `https://ipfs.io/ipfs/${window.userProfile.img}`
+      return await constructFeedEntry(account,cid, img, content)
+  }
+
+  const constructFeedEntry = async (from, cid, img, content) => {
       var box = document.createElement("div");
       box.setAttribute("id", cid);
       box.classList.add("bg-white");
       box.classList.add("border");
       box.classList.add("mt-2");
-      box.innerHTML = `<div>
+      box.innerHTML = `<div">
             <div class="d-flex flex-row justify-content-between align-items-center p-2 border-bottom">
                 <div class="d-flex flex-row align-items-center feed-text px-2">
-                    <img class="rounded-circle" src="https://i.imgur.com/aoKusnD.jpg" width="45">
+                    <img class="rounded-circle" src="${img}" width="45">
                     <div class="d-flex flex-column flex-wrap ml-2"><span class="font-weight-bold">
-                        ${account}</span><span class="text-black-50 time">40 minutes ago</span>
+                        ${from}</span><span class="text-black-50 time">40 minutes ago</span>
                     </div>
                 </div>
                 <div class="feed-icon px-2"><i class="fa fa-ellipsis-v text-black-50"></i></div>
@@ -153,10 +193,25 @@ const ethEnabled = async () => {
   }
 
   const isRegistered = async () => {
-    return await window.contract.methods.isRegistered(window.defaultAccount).call()
+    var status = await window.contract.methods.isRegistered(window.defaultAccount).call({"from":window.defaultAccount})
+    if(!status) alert({"message":"You'll have to register with this contract before you can access the api!"})
+    return status
   }
 
+  const populateUser = async () => {
+    window.userProfile = await window.contract.methods.profile(window.defaultAccount).call({"from":window.defaultAccount});
+  }
+
+ 
   // Event listeners
+  const contractEvents = async () => {
+    if (!isRegistered) return
+    let events = window.contract.events.allEvents({}, function(event){
+            console.log('Revieced Event: ' + event);
+            alert({"message": event.event})
+    });
+  }
+
   DOM.refresh().onclick = async (e) => {
     //prevent page refresh
     e.preventDefault()
@@ -167,16 +222,19 @@ const ethEnabled = async () => {
     e.preventDefault()
     // let name = DOM.fileName().value
     let content = DOM.words().value
-
+    DOM.words().value = ""
     window.cid = await store(content)
     window.lastPost = await window.contract.methods.createPost(window.cid);
+    createEventEntry("Submitting transaction...")
     console.log('Submitting transaction...');
     window.lastPost.send(
         {
             from: window.defaultAccount,
-            gas: '3000000'})
+            gas: '3000000'
+        })
     .then(function(receipt){
         console.log(receipt);
+        createEventEntry(`Transaction Complete! ${receipt.transactionHash}`);
     });
   }
 
@@ -192,18 +250,17 @@ const ethEnabled = async () => {
     });
   }
 
-
-
 if (ethEnabled) {
   // get friends
   console.log("we have eth!");
   window.ethereum.request({method: 'eth_requestAccounts'}).then(acts => {
       window.defaultAccount = acts[0];
-      
       Contract.setProvider(window.ethereum);
-      window.contract = new Contract(abi, '0xe97dE59010A39fE0913A897F5C205a85122D6b59');
+      window.contract = new Contract(abi, contractAddress);
       console.log("loading feed");
+      populateUser();
       populateFeed();
+      contractEvents();
   });
 
 }
